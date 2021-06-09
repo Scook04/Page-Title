c36c6649551fb4e942c46782b797018ec8f8ce53
Scook04 <82523877+Scook04@users.noreply.github.com>
2021-06-09 09:24:40 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,52 @@
+$(".B1").click(function() {
+  $(".pic").hide();
+  $(".title").hide();
+   $(".B1").hide();
+    $(".Q1").show();
+});
+
+$(".B2").click(function() {
+  $("h1").hide();
+  $(".B2").hide();
+   $(".B3").hide();
+     $("p").hide();
+   $(".p3").show();
+    $(".Q2").show();
+    $(".p2").show();
+});
+
+$(".B3").click(function() {
+  $("h1").hide();
+  $(".B2").hide();
+   $(".B3").hide();
+  $(".p0").hide(); 
+  $("p").hide();
+  $(".p3").show();
+  $(".Q2").show();
+  $(".p2").show();
+});
+
+$(".B4").click(function() {
+  $("h2").hide();
+  $(".B4").hide();
+   $(".B5").hide();
+   $("p").hide();
+    $(".p0").hide();
+    $(".Q3").show();
+});
+
+$(".B5").click(function() {
+  $("h2").hide();
+  $(".B4").hide();
+   $(".B5").hide();
+   $("p").hide();
+    $(".Q3").show();
+});
+
+$(".B4").click(function() {
+  $("h2").hide();
+  $(".B4").hide();
+   $(".B5").hide();
+   $("p").hide();
+    $(".p0").hide();
+});
